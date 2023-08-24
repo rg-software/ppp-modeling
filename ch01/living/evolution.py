bd93b98e1b311c214fc7e8c8def0651425b50dbc
Maxim Mozgovoy <mozgovoy@u-aizu.ac.jp>
2023-08-24 21:38:51 +0900

    evolution update

--- ch01/living/evolution.py
+++ ch01/living/evolution.py
@@ -1,8 +1,6 @@
-# chk % of fissions by generation
-
 import turtle
 from random import randint
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 H = 40  # grass patch size
 W = 70
@@ -10,28 +8,21 @@ SLEEP_MS = 20
 CELLSIZE = 10
 SHAPE_SIZE = CELLSIZE / 20
 
-INITIAL_ENERGY = 250 / 2  # 30
+INITIAL_ENERGY = 120
 MAX_ENERGY = 370
 FISSION_ENERGY = 250
 MATURITY_AGE = 200
 FOOD_ENERGY = 20
-MAX_WEIGHT = 16 * 2
-GARDEN_WIDTH = 15
-PERIOD = 3
+MAX_WEIGHT = 32
+EDEN_WIDTH = 15
+EDEN_MARGIN = 10
+PLANKTON_PERIOD = 3
 PLANKTON_COUNT = 200
+BUGS_COUNT = 10
 
-writer = turtle.Turtle()
-writer.hideturtle()
-writer.penup()
-writer.goto(0, 0)
 
-from collections import defaultdict
-
-bugs_count = defaultdict(int)  # by gen
-fis_count = defaultdict(int)  # by gen
-starved_count = defaultdict(int)
-starved_age = defaultdict(int)
-dist = defaultdict(int)
+gen_count = []
+gen_visited = []
 
 
 @dataclass
@@ -76,42 +67,45 @@ class Plankton:
         p.goto(x, y)
         p.hideturtle()
         return cls(p)
-        # p.setundobuffer(None)
-        # self.plankton[(x, y)] = p
 
 
 @dataclass
 class Bug:
     shape: turtle.Turtle
     dirweights: list
-    energy: int  # = INITIAL_ENERGY
+    energy: int
     generation: int
+    visited: set
     age: int = 0
-    visited: set = field(default_factory=set)
 
-    def coords(self):
-        return (int(self.shape.xcor()), int(self.shape.ycor()))
+    def x(self):
+        return int(self.shape.xcor())
 
-    def weighted_random(self):  # pairs):
-        cnt = 0
-        total = sum(self.dirweights)
-        r = randint(1, total)
-        for weight in self.dirweights:
+    def y(self):
+        return int(self.shape.ycor())
+
+    def weighted_random(self):
+        r = randint(1, sum(self.dirweights))
+        for cnt, weight in enumerate(self.dirweights):
             r -= weight
             if r <= 0:
                 return cnt
-            cnt += 1
-        raise RuntimeError("error")
+        raise RuntimeError()
+
+    def remove(self):
+        gen_visited[self.generation] += len(self.visited)
+        self.shape.hideturtle()
+        visits = [f"{gen_visited[i]/gen_count[i]:.2f}" for i in range(len(gen_count))]
+
+        print(f"Profile: {self.dirweights}")
+        print("Visits: " + " ".join(visits))
 
     def eat_and_move(self, food):
-        self.visited.add((int(self.shape.xcor()), int(self.shape.ycor())))
+        self.visited.add((self.x(), self.y()))
         self.age += 1
         self.energy -= 1
         if self.energy == 0:
-            starved_count[self.generation] += 1
-            starved_age[self.generation] += self.age
-            dist[self.generation] += len(self.visited)
-            self.shape.hideturtle()
+            self.remove()
         else:
             self.energy = min(MAX_ENERGY, self.energy + food)
             r = self.weighted_random()
@@ -128,46 +122,31 @@ class Bug:
 
     def fission(self):
         if self.age > MATURITY_AGE and self.energy > FISSION_ENERGY:
-            x = self.shape.xcor()
-            y = self.shape.ycor()
+            self.remove()
             e = int(self.energy / 2)
-            gen = self.generation + 1
             dirs1 = self.new_dirweights(self.dirweights, 2)
             dirs2 = self.new_dirweights(self.dirweights, 0.5)
 
-            print(dirs1)
-            print(dirs2)
-
-            self.shape.hideturtle()
-
-            dist[self.generation] += len(self.visited)
-            fis_count[self.generation] += 1
-            print("fission %")
-            for g in bugs_count:
-                print(f"{round(100*fis_count[g]/bugs_count[g],2)} ", end=" ")
-            print("\nstarved age")
-            for g in bugs_count:
-                if starved_count[g] != 0:
-                    print(f"{round(starved_age[g]/starved_count[g],2)} ", end=" ")
-            print("\navg dist")
-            for g in bugs_count:
-                print(f"{round(dist[g]/bugs_count[g],2)} ", end=" ")
-            print()
-
-            return [Bug.create(x, y, dirs1, e, gen), Bug.create(x, y, dirs2, e, gen)]
+            return [
+                Bug.create(self.x(), self.y(), dirs1, e, self.generation + 1),
+                Bug.create(self.x(), self.y(), dirs2, e, self.generation + 1),
+            ]
         return [self]
 
     @classmethod
-    def create(cls, x, y, lst, energy, gen):
-        bugs_count[gen] += 1
+    def create(cls, x, y, weights, energy, gen):
+        if gen == len(gen_count):
+            print(f"Generation: {gen}")
+            gen_count.append(0)
+            gen_visited.append(0)
+        gen_count[gen] += 1
 
         p = turtle.Turtle()
         p.penup()
         p.shape("turtle")
         p.shapesize(SHAPE_SIZE)
         p.goto(x, y)
-        print(f"Created bug gen {gen}")
-        return cls(p, lst, energy, gen)
+        return cls(p, weights, energy, gen, set())
 
     @classmethod
     def create_random(cls):
@@ -179,30 +158,28 @@ class Bug:
 
 @dataclass
 class WorldState:
-    plankton: dict
+    plankton: list  # dict
     bugs: list
     cycle: int
 
-    def add_plankton(self):
-        x, y = randint(0, W - 1), randint(0, H - 1)
-        gx, gy = randint(0, GARDEN_WIDTH), randint(0, GARDEN_WIDTH)
+    def add_plankton(self, count=1):
+        for _ in range(count):
+            x, y = randint(0, W - 1), randint(0, H - 1)
+            ex, ey = randint(EDEN_MARGIN, EDEN_WIDTH), randint(EDEN_MARGIN, EDEN_WIDTH)
 
-        self.plankton[(x, y)].show(True)
-        self.plankton[(gx, gy)].show(True)
+            self.plankton[x][y].show(True)
+            self.plankton[ex][ey].show(True)
+        return self
 
     def update(self):
-        writer.clear()
-        writer.hideturtle()
-        writer.write(f"{self.cycle} {len(turtle.turtles())}")
-
         self.cycle += 1
 
-        if self.cycle % PERIOD == 0:
+        if self.cycle % PLANKTON_PERIOD == 0:
             self.add_plankton()
 
         for b in self.bugs:
-            food = self.plankton[b.coords()]
-            b.eat_and_move(food.energy())  # food.is_shown())
+            food = self.plankton[b.x()][b.y()]
+            b.eat_and_move(food.energy())
             food.show(False)
 
         self.bugs = [b for b in self.bugs if b.energy > 0]
@@ -210,23 +187,17 @@ class WorldState:
 
     @classmethod
     def setup(cls):
-        coords = [(x, y) for x in range(W) for y in range(H)]
+        bugs = [Bug.create_random() for _ in range(BUGS_COUNT)]
 
-        bugs = [Bug.create_random() for _ in range(10)]  # bug count
-        plankton = {c: Plankton.create(c[0], c[1]) for c in coords}
+        plankton = [[Plankton.create(x, y) for y in range(H)] for x in range(W)]
 
-        r = cls(plankton, bugs, 0)
-        for _ in range(PLANKTON_COUNT):
-            r.add_plankton()
-
-        return r
+        return cls(plankton, bugs, 0).add_plankton(PLANKTON_COUNT)
 
 
 def setup_screen(title):
     turtle.setup(W * CELLSIZE, H * CELLSIZE)
     turtle.tracer(0, 0)
     turtle.title(title)
-    # turtle.setundobuffer(None)
     turtle.setworldcoordinates(0, 0, W, H)
 
 
