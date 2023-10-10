24c7507b10a1e2f179cbb84ac405ff8d315598e4
Maxim Mozgovoy <mozgovoy@u-aizu.ac.jp>
2023-10-11 00:47:31 +0900

    Galton board added

---
+++ ch01/assorted/galton.py
@@ -0,0 +1,15 @@
+from random import randint
+
+N = 100000
+PEGS = 7
+
+bins = [0 for _ in range(PEGS + 1)]
+
+for _ in range(N):
+    bin_idx = 0
+    for _ in range(PEGS):
+        if randint(0, 1) == 1:
+            bin_idx += 1
+    bins[bin_idx] += 1
+
+print(f"bins: {bins}")
